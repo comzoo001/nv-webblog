564fc4d386316cb8ff6ecebe37c2dd56dfda8d22
comzoo001 <comzoo001@gmail.com>
2020-08-12 17:42:17 +0700

    folder perpared

---
+++ client/src/components/Blogs/CreateBlog.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+<h1>Blog Creat</h1>
+</div>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -6,13 +6,20 @@ import UserIndex from '@/components/Users/Index'
 import UserCreate from '@/components/Users/CreateUser'
 import UserEdit from '@/components/Users/EditUser'
 import UserShow from '@/components/Users/ShowUser'
-
+// Comments
+import CommentIndex from '@/components/Comments/Index'
+// Blogs
+import BlogIndex from '@/components/Blogs/Index'
+import BlogCreate from '@/components/Blogs/CreateBlog'
+import BlogEdit from '@/components/Blogs/EditBlog'
+import BlogShow from '@/components/Blogs/ShowBlog'
 // Authen
 import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/Users',
@@ -40,5 +47,33 @@ export default new Router({
       name: 'login',
       component: Login
     },
+    // blogs
+    {
+      path: '/blogs',
+      name: 'blogs',
+      component: BlogIndex
+      },
+      {
+      path: '/blog/create',
+      name: 'blogs-edit',
+      component: BlogCreate
+      },
+      {
+      path: '/blog/edit/:blogId',
+      name: 'blog-edit',
+      component: BlogEdit
+      },
+      {
+      path: '/blog/:blogId',
+      name: 'blog',
+      component: BlogShow
+      },
+      // comments
+      {
+      path: '/comments',
+      name: 'comments',
+      component: CommentIndex
+      },
+
   ]
 })
